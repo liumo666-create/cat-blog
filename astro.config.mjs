5414a7075a04b3ee30c5be13996273d3b2f3f1de
liumo666 <GitHubliumo666@gmail.com>
2026-07-20 23:59:37 +0800

    fix vite bug with server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  // 把原来的 hybrid 改成 static
-  output: 'static',
+  // 开启全局 Serverless 模式，完美避开 Vite 的静态打包 Bug
+  output: 'server',
   
   // 接入 Cloudflare Pages 适配器
   adapter: cloudflare({
