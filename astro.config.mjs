5558a59c8582b0d7dc80745dc90488151a2058f7
liumo666 <GitHubliumo666@gmail.com>
2026-07-20 23:52:08 +0800

    修复了日记上传的错误

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  // 开启混合渲染模式：默认全局纯静态，只对特定的 API 或页面开启动态 SSR
-  output: 'hybrid',
+  // 把原来的 hybrid 改成 static
+  output: 'static',
   
   // 接入 Cloudflare Pages 适配器
   adapter: cloudflare({
     platformProxy: {
-      enabled: true // 允许在本地开发环境模拟读取 D1 和 R2
+      enabled: true 
     }
   })
 });
